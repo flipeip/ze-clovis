a5594cc2af139ca50e42e1f7a4c740d3f1265e8a
Felipe Matheus <felipe.mirian.fm@gmail.com>
2022-07-26 18:38:18 +0000

    Correção dos testes

--- codigo/tests/tests_config.py
+++ codigo/tests/tests_config.py
@@ -3,12 +3,12 @@ from codigo.models import Config
 
 class TestesDeConfig(TestCase):
     def setUp(self):
-        Config.get_singleton()
+        Config.get_instance()
     
     def test_singleton_is_first_entry(self):
-        singleton = Config.get_singleton()
+        singleton = Config.get_instance()
         self.assertEqual(singleton, Config.objects.first())
     
     def test_if_no_more_than_one_config_after_getting_singleton(self):
-        Config.get_singleton()
+        Config.get_instance()
         self.assertEqual(len(Config.objects.all()), 1)
